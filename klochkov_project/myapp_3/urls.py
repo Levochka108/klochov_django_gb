f05684d1e7e108512233971f9842c6bb650e1cce
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2024-02-29 16:10:36 +0300

    закончил работу с эти приложерем

--- klochkov_project/myapp_3/urls.py
+++ klochkov_project/myapp_3/urls.py
@@ -3,6 +3,7 @@ from .views import hello, HelloView
 from .views import TemplIf
 from .views import view_for
 from .views import index, about
+from .views import author_posts, post_full
 
 urlpatterns = [
     path('hello/', hello, name='hello'),
@@ -11,4 +12,6 @@ urlpatterns = [
     path('for/', view_for, name='templ_for'),
     path('index/', index, name='idex'),
     path('about/', about, name='about'),
+    path('author/<int:author_id>', author_posts, name='author_posts'),
+    path('post/<int:post_id>/', post_full, name='post_full'),
 ]
