e226b0aeebb1842dec98f12b2c8915f05076ef80
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2024-02-29 17:24:34 +0300

    1

--- klochkov_project/myapp_4/forms.py
+++ klochkov_project/myapp_4/forms.py
@@ -13,7 +13,7 @@ class ManyFieldsForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     age = forms.IntegerField(min_value=0, max_value=120)
-    heigth = forms.FileField()
+    heigth = forms.FloatField()
     is_active = forms.BooleanField(required=False)
     birthdate = forms.DateField(initial=datetime.date.today)
     gender = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')])
